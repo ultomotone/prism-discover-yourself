5c6cde16257cd82d394e65ad19e585f5841e94aa
Daniel Speiss <daniel.joseph.speiss@gmail.com>
2025-09-07 14:59:31 -0400

    fix: grant anon access to recent assessments view

--- supabase/policies/anon_dashboard_policies.sql
+++ supabase/policies/anon_dashboard_policies.sql
@@ -1,5 +1,6 @@
 -- Example: allow SELECT on safe views for anon
 grant usage on schema public to anon;
-grant select on public.v_latest_assessments_v11, public.v_kpi_overview_30d_v11 to anon;
+grant select on public.v_latest_assessments_v11, public.v_kpi_overview_30d_v11, public.v_recent_assessments_safe to anon;
+grant execute on function public.get_recent_assessments_safe() to anon;
 
 -- If you have RLS ON views (rare), use SECURITY INVOKER functions instead; but views above should be safe.
