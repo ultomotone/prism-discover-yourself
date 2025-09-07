2848d07df5dc6efb37cb5c289335656b549866b6
Daniel Speiss <daniel.joseph.speiss@gmail.com>
2025-09-07 15:52:27 -0400

    chore(sql): alphabetize anon view grants

--- supabase/policies/anon_dashboard_policies.sql
+++ supabase/policies/anon_dashboard_policies.sql
@@ -1,8 +1,8 @@
 -- Example: allow SELECT on safe views for anon
 grant usage on schema public to anon;
 grant select on
-  public.v_latest_assessments_v11,
   public.v_kpi_overview_30d_v11,
+  public.v_latest_assessments_v11,
   public.v_recent_assessments_safe
 to anon;
 grant execute on function public.get_recent_assessments_safe() to anon;
